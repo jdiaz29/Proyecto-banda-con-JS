8b905fff0fe20a0c3a762b9dae168b0a5eac6efe
javier diaz <javi.d.diaz@gmail.com>
2021-08-28 02:06:46 -0300

    algoritmo con condicional

--- Desktop/practica visual/cm.js
+++ Desktop/practica visual/cm.js
@@ -2,4 +2,16 @@ var textoingresado = prompt("Ingresá tu nombre:");
 
 var saludo = "Bienvenide a CuartoMenguante " + textoingresado;
 
-alert( saludo);
+alert( saludo); 
+
+/* CONDICIONAL */
+var edad = prompt ("¿Cuántos años tenés?");
+
+if (edad >=18) {
+
+    alert ("Puedes ver el contenido");}
+
+
+else{ (edad <18) 
+    alert ("Lo sentimos, no puedes ver este contenido");
+}
