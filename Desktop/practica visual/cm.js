30286dec39e9ec9531b6630eca718e67d1c0b664
javier diaz <javi.d.diaz@gmail.com>
2021-08-31 23:32:10 -0300

    practica ciclos

--- Desktop/practica visual/cm.js
+++ Desktop/practica visual/cm.js
@@ -15,3 +15,34 @@ if (edad >=18) {
 else{ (edad <18) 
     alert ("Lo sentimos, no puedes ver este contenido");
 }
+
+
+/* CICLOS  */
+
+ /* for */
+
+for (i = 0; i < 5; i++) {
+let cantidadiscos = parseInt(prompt("Ingrese la cantidad de discos que desea comprar"));
+
+if (cantidadiscos <= 5) {
+    alert ( "Gracias por su compra tiene " + cantidadiscos + " discos reservados en su carrito." );
+break
+}}
+
+
+let discos = prompt ("Cantidad de discos que desea comprar");
+
+/*while*/
+
+while (discos <=5) {
+
+alert ( "Gracias por su compra tiene " + discos +" discos reservados en su carrito.");
+
+ discos = prompt ("Cantidad de discos que desea comprar");
+
+}
+
+
+
+
+
