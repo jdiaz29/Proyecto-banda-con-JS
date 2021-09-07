df2f4bac91830f269ebcaa67c83fd7a1b87c194d
javier diaz <javi.d.diaz@gmail.com>
2021-09-06 21:14:17 -0300

    practica funciones

--- Desktop/practica visual/cm.js
+++ Desktop/practica visual/cm.js
@@ -17,30 +17,36 @@ else{ (edad <18)
 }
 
 
-/* CICLOS  */
 
- /* for */
-
-for (i = 0; i < 5; i++) {
-let cantidadiscos = parseInt(prompt("Ingrese la cantidad de discos que desea comprar"));
-
-if (cantidadiscos <= 5) {
-    alert ( "Gracias por su compra tiene " + cantidadiscos + " discos reservados en su carrito." );
-break
-}}
-
-
+/* Funcion multiplicar */
 let discos = prompt ("Cantidad de discos que desea comprar");
+let valordisco = 150;
+let resultado = 0;
 
-/*while*/
 
-while (discos <=5) {
+function calcular(discos,valordisco,operacion) {
+  resultado = (discos * valordisco)
+           }
 
-alert ( "Gracias por su compra tiene " + discos +" discos reservados en su carrito.");
+calcular(discos,valordisco)
+alert ("Son $"+resultado);
 
- discos = prompt ("Cantidad de discos que desea comprar");
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
